6837832dffdcf2ed713d5810c5ccc7fd38bdaadb
Johnny Backlund <johnnybacklund@hotmail.com>
2021-08-30 17:38:52 +0200

    Mail notification animations now work

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import { PaperPlane } from "./static/icons";
 import emailjs from "emailjs-com";
 import { init } from "emailjs-com";
 import { BeatLoader } from "react-spinners";
+import { Transition } from "react-transition-group";
 
 function ContactForm() {
   const user = process.env.REACT_APP_USER_ID;
@@ -14,6 +14,7 @@ function ContactForm() {
   const [message, setMessage] = useState();
   const [messageSuccess, setMessageSuccess] = useState(false);
   const [messageFailed, setMessageFailed] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (messageSuccess) {
@@ -43,6 +44,7 @@ function ContactForm() {
     } else if (!emailInRightFormat) {
       alert("Please enter a valid email.");
     } else {
+      setLoading(true);
       const templateParams = {
         user_name: name,
         user_email: email,
@@ -53,10 +55,12 @@ function ContactForm() {
         (response) => {
           setMessageSuccess(true);
           console.log("SUCCESS!", response.status, response.text);
+          setLoading(false);
         },
         (error) => {
           setMessageFailed(true);
           console.log("FAILED...", error);
+          setLoading(false);
         }
       );
       setName("");
@@ -66,17 +70,43 @@ function ContactForm() {
     }
   }
 
-  function MessageFailed() {
+  const duration = 300;
+
+  const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 1,
+  };
+
+  const transitionStyles = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 1 },
+    exited: { opacity: 0 },
+  };
+
+  function MessageFailed(props) {
     return (
-      <div className="absolute transition-all rounded text-red-900 transform bg-red-400 text-center -translate-y-12 top-1/2 right-0 left-0 mx-auto w-44 py-4 text-lg font-bold">
+      <div
+        style={{
+          ...defaultStyle,
+          ...transitionStyles[props.state],
+        }}
+        className="absolute transition-all rounded text-red-900 transform bg-red-400 text-center -translate-y-12 top-1/2 right-0 left-0 mx-auto w-44 py-4 text-lg font-bold"
+      >
         Mail failed
       </div>
     );
   }
 
-  function MessageSuccess() {
+  function MessageSuccess(props) {
     return (
-      <div className="absolute transition-all rounded text-green-800 transform bg-green-400 text-center -translate-y-12 top-1/2 right-0 left-0 mx-auto w-32 py-4 text-xl font-bold">
+      <div
+        style={{
+          ...defaultStyle,
+          ...transitionStyles[props.state],
+        }}
+        className="absolute rounded text-green-800 transform bg-green-400 text-center -translate-y-12 top-1/2 right-0 left-0 mx-auto w-32 py-4 text-xl font-bold"
+      >
         Mail sent!
       </div>
     );
@@ -87,8 +117,17 @@ function ContactForm() {
       className="flex flex-col text-base bg-blue-900 pt-5 px-5 pb-3 rounded relative z-50 text-white"
       onSubmit={(e) => sendEmail(e)}
     >
-      {messageSuccess && <MessageSuccess />}
-      {messageFailed && <MessageFailed />}
+      <Transition in={messageSuccess} timeout={300}>
+        {(state) => <MessageSuccess state={state} />}
+      </Transition>
+      <Transition in={messageFailed} timeout={300}>
+        {(state) => <MessageFailed state={state} />}
+      </Transition>
+      {loading && (
+        <div className="absolute transform text-center top-1/2 right-0 left-0 w-20 mx-auto">
+          <BeatLoader color="aqua" loading={true} />
+        </div>
+      )}
       <div className="graybgdiv absolute inset-0 bg-gray-900 bg-opacity-10 rounded"></div>
       <div className="flex">
         <label for="name" className=""></label>
@@ -119,7 +158,6 @@ function ContactForm() {
         placeholder="Your message here..."
         onChange={(e) => setMessage(e.target.value)}
       ></textarea>
-
       <button
         className="transition-all bg-blue-600 hover:bg-blue-500 px-6 py-3 rounded mt-3 mx-auto flex font-semibold"
         type="submit"
